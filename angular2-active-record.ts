a1045b93ed0ae1042adb25ea95559c7fd81b5780
Andrii Votiakov <andrii@enginearch.com>
2017-02-08 10:37:56 +0100

    add IBApiConfig type to ApiConfig constructor argument; small refactor for TS linter

--- angular2-active-record.ts
+++ angular2-active-record.ts
@@ -1,46 +1,46 @@
-import {Injectable}                      from '@angular/core';
-import {Response}                        from '@angular/http';
-import {Headers, RequestOptions}         from '@angular/http';
-import 'rxjs/add/operator/toPromise';
+import {Injectable}                      from "@angular/core";
+import {Response}                        from "@angular/http";
+import {Headers, RequestOptions}         from "@angular/http";
+import "rxjs/add/operator/toPromise";
 export interface MethodHttp {
-  query: string;
-  update: string;
-  insert: string;
-  delete: string;
+  query?: string;
+  update?: string;
+  insert?: string;
+  delete?: string;
   [method: string]: string;
 }
 export interface IBApiConfig {
-  urlAPI: string;
-  headers: any;
-  methods: MethodHttp;
+  urlAPI?: string;
+  headers?: any;
+  methods?: MethodHttp;
 }
 export class ApiConfig {
-  config: any;
-  urlAPI: string;
-  headers: any;
-  methods: MethodHttp;
-  defaultMethods: MethodHttp = {
+  public urlAPI: string;
+  public headers: any;
+  public methods: MethodHttp;
+  public defaultMethods: MethodHttp = {
     query: "get",
     update: "put",
     insert: "post",
     delete: "delete"
   };
-  constructor(config?: any) {
-    this.config = config || {};
-    this.urlAPI = this.config.urlAPI || "http://localhost:3456/api/"
-    this.headers = this.config.headers || {};
-    this.methods = this.config.methods || {};
-    let method: string;
-    for (method in this.defaultMethods) {
-      this.methods[method] = this.methods[method] || this.defaultMethods[method];
+  constructor(config?: IBApiConfig) {
+    config = config || <any> {};
+    this.urlAPI = config.urlAPI || "http://localhost:3456/api/";
+    this.headers = config.headers || {};
+    this.methods = config.methods || <any> {};
+    for (const method in this.defaultMethods) {
+      if (this.defaultMethods.hasOwnProperty(method)) {
+        this.methods[method] = this.methods[method] || this.defaultMethods[method];
+      }
     }
   }
-  getConfig() {
+  public getConfig() {
     return {
       urlAPI: this.urlAPI,
       headers: this.headers,
       methods: this.methods
-    }
+    };
   }
 }
 export class ActiveRecord<T> {
@@ -51,75 +51,75 @@ export class ActiveRecord<T> {
     this.api_url = this._config.urlAPI + "" + table_name;
   }
   // Ex:[GET] /${table_name}?page=1&sort=title
-  findAll(params: any = { page: 1, sort: "" }): Promise<T[]> {
+  public findAll(params: any = { page: 1, sort: "" }): Promise<T[]> {
     return this.httpService[this._config.methods.query](this.api_url + this.generateParam(params))
       .toPromise()
       .then((res: Response) => this.processData(res))
       .catch(this.handleError);
   }
   // Ex:[GET] /${table_name}/search?title=abc&page=1&sort=title
-  search(data: any, api_search_name: string = ""): Promise<T[]> {
+  public search(data: any, api_search_name: string = ""): Promise<T[]> {
     return this.httpService[this._config.methods.query](this.api_url + "/" + api_search_name + this.generateParam(data))
       .toPromise()
       .then((res: Response) => this.processData(res))
       .catch(this.handleError);
   }
   // Ex:[GET] /${table_name}/${id}
-  find(id: any): Promise<T> {
+  public find(id: any): Promise<T> {
     return this.httpService[this._config.methods.query](this.api_url + "/" + id)
       .toPromise()
       .then((res: Response) => this.processData(res))
       .catch(this.handleError);
   }
   // Ex:[GET] /${table_name}/${id}
-  update(id: any, data: any) {
-    let body = JSON.stringify(data);
-    if (!this._config.headers['Content-Type']) {
-      this._config.headers['Content-Type'] = 'application/json';
+  public update(id: any, data: any) {
+    const body = JSON.stringify(data);
+    if (!this._config.headers["Content-Type"]) {
+      this._config.headers["Content-Type"] = "application/json";
     }
-    let headers = new Headers(this._config.headers);
-    let options = new RequestOptions({ headers: headers });
+    const headers = new Headers(this._config.headers);
+    const options = new RequestOptions({ headers });
     return this.httpService[this._config.methods.update](this.api_url + "/" + id, body, options)
       .toPromise()
       .then((res: Response) => this.processData(res))
       .catch(this.handleError);
   }
   // Ex:[GET] /${table_name}
-  insert(data: any) {
-    let body = JSON.stringify(data);
-    if (!this._config.headers['Content-Type']) {
-      this._config.headers['Content-Type'] = 'application/json';
+  public insert(data: any) {
+    const body = JSON.stringify(data);
+    if (!this._config.headers["Content-Type"]) {
+      this._config.headers["Content-Type"] = "application/json";
     }
-    let headers = new Headers(this._config.headers);
-    let options = new RequestOptions({ headers: headers });
+    const headers = new Headers(this._config.headers);
+    const options = new RequestOptions({ headers });
     return this.httpService[this._config.methods.insert](this.api_url, body, options)
       .toPromise()
       .then((res: Response) => this.processData(res))
       .catch(this.handleError);
   }
   // Ex:[GET] /${table_name}/${id}
-  delete(id: any) {
-    let headers = new Headers(this._config.headers);
-    let options = new RequestOptions({ headers: headers });
+  public delete(id: any) {
+    const headers = new Headers(this._config.headers);
+    const options = new RequestOptions({ headers });
     return this.httpService[this._config.methods.delete](this.api_url + "/" + id, options)
       .toPromise()
       .then((res: Response) => this.processData(res))
       .catch(this.handleError);
   }
   protected generateParam(params: any = {}): string {
-    let params_arr: Array<string> = [];
-    for (let key in params) {
+    const paramsArr: string[] = [];
+    for (const key in params) {
       if (params[key]) {
-        params_arr.push(key + "=" + params[key]);
+        paramsArr.push(key + "=" + params[key]);
       }
     }
-    return "?" + params_arr.join("&");
+    return "?" + paramsArr.join("&");
   }
   protected processData(res: Response) {
-    return <T>res.json();
+    return <T> res.json();
   }
-  protected handleError(error: any): Promise<any>{
-    console.error('An error occurred', error);
+  protected handleError(error: any): Promise<any> {
+    console.error("An error occurred", error);
     return Promise.reject(error.message || error);
   }
 }
